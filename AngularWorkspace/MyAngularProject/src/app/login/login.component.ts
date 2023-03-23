52890517c9ebcc84c31115518e7cf4f57c99952a
kokaneva <kokane.vina@gmail.com>
2023-03-23 15:50:01 +0530

    Event Binding

--- AngularWorkspace/MyAngularProject/src/app/login/login.component.ts
+++ AngularWorkspace/MyAngularProject/src/app/login/login.component.ts
@@ -8,9 +8,29 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   username="";
   password="";
-
+  age=0;
   collectData(){
     console.log(this.username);
     console.log(this.password);
+  }
+
+  collectData2(un:any,ps:any,age:any){
+    console.log(un);
+    console.log(typeof un);
+    
+    console.log(ps);
+    console.log(typeof ps);
+
+    console.log(age);
+    console.log(typeof age);
+  }
+
+  collectData3(logForm:any){
+    console.log("hi"+this.username);
+    console.log("hi"+this.password);
+    console.log(logForm); // pass this logForm at backend to check validity of user
+    console.log(logForm.value.age);
+    console.log(logForm.value.username);
+    console.log(logForm.value.password);
   }
 }

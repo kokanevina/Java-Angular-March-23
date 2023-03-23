1266555f039c396754d5e8fac5fc0d5f0c44cddb
kokaneva <kokane.vina@gmail.com>
2023-03-23 13:13:18 +0530

    Components

--- AngularWorkspace/MyAngularProject/src/app/app.module.ts
+++ AngularWorkspace/MyAngularProject/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavbarComponent  // components, pipes, 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, FormsModule  // other modules, builtin, custom modules
   ],
-  providers: [],
+  providers: [ 
+      // custom services
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

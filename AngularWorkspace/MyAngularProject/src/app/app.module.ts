d36e4ec75bd2fa8069a9e195c19e2c3227bd6086
kokaneva <kokane.vina@gmail.com>
2023-03-30 15:14:53 +0530

    Final

--- AngularWorkspace/MyAngularProject/src/app/app.module.ts
+++ AngularWorkspace/MyAngularProject/src/app/app.module.ts
@@ -7,13 +7,11 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
-import { TestcomponentComponent } from './testcomponent/testcomponent.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { OrderModule } from 'ngx-order-pipe';
 import { LengthPipe } from './CustomPipes/length.pipe';
 import { MypipePipe } from './mypipe.pipe' ;
-import { TestService } from './test.service';
 import { CharatPipe } from './CustomPipes/charat.pipe';
 import { RegisterComponent } from './register/register.component';
 import { AuthorDetailsComponent } from './author-details/author-details.component';
@@ -30,7 +28,6 @@ import { CounterComponentComponent } from './counter-component/counter-component
     HeaderComponent,
     NavbarComponent,
     HomeComponent,
-    TestcomponentComponent,
     LoginComponent,
     LengthPipe,
     MypipePipe,
